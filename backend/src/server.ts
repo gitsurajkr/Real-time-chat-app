1dce4a771c2cb068caf0441e41d376eb95f737ec
Suraj Kumar <kumarsuraj.islampur@gmail.com>
2025-07-28 18:21:00 +0530

    handle image json string

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -61,20 +61,26 @@ wss.on("connection", function connection(ws) {
     console.log(`[CONNECTED] user ${id}`);
 
     ws.on("message", async (data, isBinary) => {
-       
-        if(isBinary){
-            
+
+        if (isBinary) {
+
             const roomid = subscription[id].rooms[0];
-            if(!roomid) return;
+            if (!roomid) return;
 
             Object.entries(subscription).forEach(([uid, { ws, rooms }]) => {
                 if (rooms.includes(roomid) && ws.readyState === WebSocket.OPEN) {
-                    ws.send(data); 
+                    ws.send(data);
                 }
             });
             return;
+
         }
-         const parsedMessage = JSON.parse(data.toString())
+        let parsedMessage;
+        try {
+            parsedMessage = JSON.parse(data.toString());
+        } catch (e) {
+            console.error("Received non-JSON message:", e);
+        }
         const type = parsedMessage.type;
 
         if (type === "SUBSCRIBE") {
@@ -85,26 +91,26 @@ wss.on("connection", function connection(ws) {
             }
             if (atLeaseOneUserConnected(room)) {
                 console.log("subscribing to pub sub to room: ", room)
-                if(!redisSubscribedRooms.has(room)){
+                if (!redisSubscribedRooms.has(room)) {
                     redisSubscribedRooms.add(room)
                     await subscribeClient.subscribe(room, (message) => {
-                    const parsed = JSON.parse(message)
-                    const { roomId, message: msg } = parsed
+                        const parsed = JSON.parse(message)
+                        const { roomId, message: msg } = parsed
 
-                    // broadcast to all useer in that particular room
-                    Object.entries(subscription).forEach(([uid, { ws, rooms }]) => {
-                        if (rooms.includes(roomId)) {
-                            ws.send(JSON.stringify({
-                                type: "RECEIVER_MESSAGE",
-                                roomId,
-                                message: msg
-                            }))
-                        }
+                        // broadcast to all useer in that particular room
+                        Object.entries(subscription).forEach(([uid, { ws, rooms }]) => {
+                            if (rooms.includes(roomId)) {
+                                ws.send(JSON.stringify({
+                                    type: "RECEIVER_MESSAGE",
+                                    roomId,
+                                    message: msg
+                                }))
+                            }
+                        })
+
                     })
-
-                })
                 }
-                
+
             }
         }
         if (type === "UNSUBSCRIBE") {
@@ -135,28 +141,28 @@ wss.on("connection", function connection(ws) {
         if (type === "USER_JOIN") {
             const roomId = parsedMessage.roomId;
             const username = parsedMessage.username;
-            
+
             if (roomId && username) {
                 subscription[id].username = username;
-                
+
                 // Initialize room presence if it doesn't exist
                 if (!userPresence[roomId]) {
                     userPresence[roomId] = {};
                 }
-                
+
                 // Mark user as online
                 userPresence[roomId][username] = {
                     lastSeen: Date.now(),
                     isOnline: true
                 };
-                
+
                 // Broadcast to other users in the room
                 broadcastToRoom(roomId, {
                     type: "USER_JOINED",
                     roomId,
                     username
                 });
-                
+
                 console.log(`User ${username} joined room ${roomId}`);
             }
         }
@@ -164,17 +170,17 @@ wss.on("connection", function connection(ws) {
         if (type === "USER_LEAVE") {
             const roomId = parsedMessage.roomId;
             const username = parsedMessage.username;
-            
+
             if (roomId && username && userPresence[roomId] && userPresence[roomId][username]) {
                 userPresence[roomId][username].isOnline = false;
                 userPresence[roomId][username].lastSeen = Date.now();
-                
+
                 broadcastToRoom(roomId, {
                     type: "USER_LEFT",
                     roomId,
                     username
                 });
-                
+
                 console.log(`User ${username} left room ${roomId}`);
             }
         }
@@ -183,20 +189,20 @@ wss.on("connection", function connection(ws) {
         if (type === "HEARTBEAT") {
             const roomId = parsedMessage.roomId;
             const username = parsedMessage.username;
-            
+
             if (roomId && username) {
                 if (!userPresence[roomId]) {
                     userPresence[roomId] = {};
                 }
-                
+
                 const wasOffline = !userPresence[roomId][username]?.isOnline;
-                
+
                 // Update user's last seen timestamp and mark as online
                 userPresence[roomId][username] = {
                     lastSeen: Date.now(),
                     isOnline: true
                 };
-                
+
                 // If user was offline and now sending heartbeat, notify others they're back online
                 if (wasOffline) {
                     broadcastToRoom(roomId, {
@@ -213,7 +219,7 @@ wss.on("connection", function connection(ws) {
         if (type === "USER_TYPING") {
             const roomId = parsedMessage.roomId;
             const username = parsedMessage.username;
-            
+
             if (roomId && username) {
                 // Broadcast typing event to other users in the room (excluding sender)
                 broadcastToRoom(roomId, {
@@ -221,7 +227,7 @@ wss.on("connection", function connection(ws) {
                     roomId,
                     username
                 }, id); // Pass sender's ID to exclude them
-                
+
                 console.log(`User ${username} is typing in room ${roomId}`);
             }
         }
@@ -229,52 +235,52 @@ wss.on("connection", function connection(ws) {
         if (type === "USER_STOP_TYPING") {
             const roomId = parsedMessage.roomId;
             const username = parsedMessage.username;
-            
+
             if (roomId && username) {
                 // Broadcast stop typing event to other users in the room (excluding sender)
                 broadcastToRoom(roomId, {
                     type: "USER_STOP_TYPING",
                     roomId,
                     username
-                }, id); 
-                
+                }, id);
+
                 console.log(`User ${username} stopped typing in room ${roomId}`);
             }
         }
     })
 
     ws.on("close", async () => {
-    console.log(`[DISCONNECTED] user ${id}`);
+        console.log(`[DISCONNECTED] user ${id}`);
 
-    const userRooms = subscription[id].rooms;
-    const username = subscription[id].username;
-    
-    // Mark user as offline in all rooms they were in
-    if (username) {
-        for (const roomId of userRooms) {
-            if (userPresence[roomId] && userPresence[roomId][username]) {
-                userPresence[roomId][username].isOnline = false;
-                userPresence[roomId][username].lastSeen = Date.now();
-                
-                broadcastToRoom(roomId, {
-                    type: "USER_LEFT",
-                    roomId,
-                    username
-                });
+        const userRooms = subscription[id].rooms;
+        const username = subscription[id].username;
+
+        // Mark user as offline in all rooms they were in
+        if (username) {
+            for (const roomId of userRooms) {
+                if (userPresence[roomId] && userPresence[roomId][username]) {
+                    userPresence[roomId][username].isOnline = false;
+                    userPresence[roomId][username].lastSeen = Date.now();
+
+                    broadcastToRoom(roomId, {
+                        type: "USER_LEFT",
+                        roomId,
+                        username
+                    });
+                }
             }
         }
-    }
-    
-    delete subscription[id];
 
-    for (const room of userRooms) {
-        if (noOneIsConnected(room)) {
-            console.log(`Unsubscribing from room ${room} (after disconnect)`);
-            await subscribeClient.unsubscribe(room);
-            redisSubscribedRooms.delete(room)
+        delete subscription[id];
+
+        for (const room of userRooms) {
+            if (noOneIsConnected(room)) {
+                console.log(`Unsubscribing from room ${room} (after disconnect)`);
+                await subscribeClient.unsubscribe(room);
+                redisSubscribedRooms.delete(room)
+            }
         }
-    }
-});
+    });
 
 })
 
@@ -289,22 +295,22 @@ function broadcastToRoom(roomId: string, message: any, excludeId?: string) {
 setInterval(() => {
     const now = Date.now();
     const OFFLINE_THRESHOLD = 30000; // 30 seconds
-    
+
     Object.keys(userPresence).forEach(roomId => {
         Object.keys(userPresence[roomId]).forEach(username => {
             const user = userPresence[roomId][username];
-            
+
             if (user.isOnline && (now - user.lastSeen) > OFFLINE_THRESHOLD) {
                 user.isOnline = false;
                 user.lastSeen = now;
-                
+
                 // Broadcast that user went offline
                 broadcastToRoom(roomId, {
                     type: "USER_OFFLINE",
                     roomId,
                     username
                 });
-                
+
                 console.log(`User ${username} marked offline in room ${roomId} due to inactivity`);
             }
         });
@@ -317,7 +323,7 @@ function atLeaseOneUserConnected(roomid: string) {
 }
 
 function noOneIsConnected(roomId: string) {
-   return !Object.values(subscription).some(sub => sub.rooms.includes(roomId))
+    return !Object.values(subscription).some(sub => sub.rooms.includes(roomId))
 }
 
 
